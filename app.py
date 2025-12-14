8fb12a0157c90dff62d9a6ae78019505dbcf7a6d
byportinhos <caiovieiraportoo@gmail.com>
2025-12-14 19:58:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title="Advogado AI - Final", layout="wide", page_icon="�
 # 1. Configurar Gemini
 try:
     genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-    model = genai.GenerativeModel('gemini-1.5-flash') 
+    model = genai.GenerativeModel('gemini-2.5-flash') 
 except:
     st.error("ERRO: Configure a GOOGLE_API_KEY nos Secrets.")
     st.stop()
@@ -249,3 +249,4 @@ elif menu == "3. Jurimetria (Investigação)":
                 else: st.warning("Digite o Nome.")
         else: st.warning("Sem clientes.")
     except Exception as e: st.error(str(e))
+
